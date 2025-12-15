b9be5387bad88df5661b8cf605def1730710e3df
LaMaxMenace <maxence.vast@free.fr>
2025-12-15 16:13:24 +0100

    feat/add template base + home

---
+++ src/Controller/Home/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Home;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
+
+#[Route('/', name: 'home', methods: ['GET'])]
+class HomeController {
+    public function __invoke(Environment $twig) : Response
+    {
+        return new Response($twig->render('home/home.html.twig', []), Response::HTTP_OK);
+    }
+}
