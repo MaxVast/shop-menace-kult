c0d276863d8d08c1d4de0397a72daec154f73f71
LaMaxMenace <maxence.vast@free.fr>
2026-08-18 12:09:48 +0200

    (chore): remove depedencies  no use

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,12 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product\Category;
 use App\Entity\Product\Product;
-use App\Entity\User\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
